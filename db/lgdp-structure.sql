88b33d5fa0fe0b7b885b0a55887ff94f065e1603
myouuu <meriem.mrabent@hetic.net>
2020-04-21 23:37:07 +0200

    :open_file_folder: update database

--- db/lgdp-structure.sql
+++ db/lgdp-structure.sql
@@ -1,23 +1,104 @@
-CREATE TABLE `images` (
-  `image_id` integer PRIMARY KEY,
-  `file_name` varchar(255),
-  `file_url` varchar(255),
-  `description` varchar(255),
-  `category_id` integer,
-  `created_at` timestamp
-);
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3308
+-- Généré le :  mar. 21 avr. 2020 à 21:26
+-- Version du serveur :  8.0.18
+-- Version de PHP :  7.2.25
 
-CREATE TABLE `category` (
-  `category_id` integer PRIMARY KEY,
-  `category_name` varchar(255)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE `assoc_tags_images` (
-  `assoc_id` integer PRIMARY KEY,
-  `image_id` integer,
-  `tag_name` varchar(255)
-);
 
-CREATE TABLE `tags` (
-  `tag_name` varchar(255) PRIMARY KEY
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `casa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `assoc_tags_images`
+--
+
+DROP TABLE IF EXISTS `assoc_tags_images`;
+CREATE TABLE IF NOT EXISTS `assoc_tags_images` (
+  `image_id` int(11) NOT NULL,
+  `tag_name` int(11) NOT NULL,
+  `assoc_id` int(11) NOT NULL,
+  PRIMARY KEY (`assoc_id`),
+  KEY `fk_image` (`image_id`),
+  KEY `fk_tag` (`tag_name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE IF NOT EXISTS `category` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `images`
+--
+
+DROP TABLE IF EXISTS `images`;
+CREATE TABLE IF NOT EXISTS `images` (
+  `image_id` int(11) NOT NULL AUTO_INCREMENT,
+  `file_name` varchar(255) DEFAULT NULL,
+  `file_url` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`image_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `images`
+--
+
+INSERT INTO `images` (`image_id`, `file_name`, `file_url`, `description`, `category_id`, `created_at`) VALUES
+(1, 'adobe', 'https://as1.ftcdn.net/jpg/02/12/43/28/500_F_212432820_Zf6CaVMwOXFIylDOEDqNqzURaYa7CHHc.jpg', 'hello it\'s me', 1, '2020-04-14 22:00:00'),
+(2, NULL, NULL, NULL, NULL, '2020-04-20 17:08:47'),
+(3, 'adobe', 'https://as1.ftcdn.net/jpg/02/12/43/28/500_F_212432820_Zf6CaVMwOXFIylDOEDqNqzURaYa7CHHc.jpg', 'hello it\'s me', NULL, '2020-04-20 17:13:46'),
+(4, 'adobe', 'https://as1.ftcdn.net/jpg/02/12/43/28/500_F_212432820_Zf6CaVMwOXFIylDOEDqNqzURaYa7CHHc.jpg', 'hello it\'s me', NULL, '2020-04-20 17:14:01'),
+(5, 'adobe', 'https://as1.ftcdn.net/jpg/02/12/43/28/500_F_212432820_Zf6CaVMwOXFIylDOEDqNqzURaYa7CHHc.jpg', 'hello it\'s me', NULL, '2020-04-20 20:20:26'),
+(6, 'adobe', 'https://as1.ftcdn.net/jpg/02/12/43/28/500_F_212432820_Zf6CaVMwOXFIylDOEDqNqzURaYa7CHHc.jpg', 'hello it\'s me', NULL, '2020-04-20 20:21:17'),
+(7, 'adobe', 'https://as1.ftcdn.net/jpg/02/12/43/28/500_F_212432820_Zf6CaVMwOXFIylDOEDqNqzURaYa7CHHc.jpg', 'hello it\'s me', NULL, '2020-04-14 22:00:00'),
+(8, 'adobe', 'https://as1.ftcdn.net/jpg/02/12/43/28/500_F_212432820_Zf6CaVMwOXFIylDOEDqNqzURaYa7CHHc.jpg', 'hello it\'s me', NULL, '2020-04-20 20:32:01'),
+(9, 'adobe', 'https://as1.ftcdn.net/jpg/02/12/43/28/500_F_212432820_Zf6CaVMwOXFIylDOEDqNqzURaYa7CHHc.jpg', 'hello it\'s me', NULL, '2020-04-21 09:09:13'),
+(10, 'adobe', 'https://as1.ftcdn.net/jpg/02/12/43/28/500_F_212432820_Zf6CaVMwOXFIylDOEDqNqzURaYa7CHHc.jpg', 'hello it\'s me', NULL, '2020-04-21 09:10:02'),
+(11, 'adobe', 'https://as1.ftcdn.net/jpg/02/12/43/28/500_F_212432820_Zf6CaVMwOXFIylDOEDqNqzURaYa7CHHc.jpg', 'hello it\'s megg', NULL, '2020-04-21 16:16:13'),
+(12, 'adobe', 'https://as1.ftcdn.net/jpg/02/12/43/28/500_F_212432820_Zf6CaVMwOXFIylDOEDqNqzURaYa7CHHc.jpg', 'hello it\'s megg', NULL, '2020-04-21 16:47:46');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tags`
+--
+
+DROP TABLE IF EXISTS `tags`;
+CREATE TABLE IF NOT EXISTS `tags` (
+  `tag_name` varchar(155) NOT NULL,
+  PRIMARY KEY (`tag_name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
