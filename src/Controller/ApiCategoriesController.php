ef5a0d5621d6a5c21c3d464c0fdb42e426cd7f2c
tsabot <tsabot.pro@gmail.com>
2020-04-25 13:13:47 +0200

    :construction: Added /categories route, db fetch

---
+++ src/Controller/ApiCategoriesController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+use App\Entity\Category;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+use Symfony\Component\Serializer\Exception\NotEncodableValueException;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManager;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+
+
+class ApiCategoriesController extends AbstractController
+{
+    /**
+     * @Route("/api/categories", name="api_category_index", methods={"GET"})
+     */
+    public function index(CategoryRepository $categoryRepository)
+    {
+        return $this->json($categoryRepository->findAll(), 200, [], ['groups' => 'category:read']);
+    }
+}
