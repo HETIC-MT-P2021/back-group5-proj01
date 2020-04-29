747372d397ed49147502cee248ff4c0170f68430
Jean-Jacques <akakpo.jeanjacques@gmail.com>
2020-04-29 15:06:26 +0200

    🗃 Update database structure and data scripts

--- db/lgdp-structure.sql
+++ db/lgdp-structure.sql
@@ -1,109 +1,120 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 4.9.3
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3308
--- Généré le :  mar. 21 avr. 2020 à 21:26
--- Version du serveur :  8.0.18
--- Version de PHP :  7.2.25
+-- Host: localhost:3306
+-- Generation Time: Apr 29, 2020 at 01:05 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.4.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Base de données :  `casa`
+-- Database: `lgdp-data`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `assoc_tags_images`
+-- Table structure for table `assoc_tags_images`
 --
 
-DROP TABLE IF EXISTS `assoc_tags_images`;
-CREATE TABLE IF NOT EXISTS `assoc_tags_images` (
-  `image_id` int(11) NOT NULL,
-  `tag_name` int(11) NOT NULL,
+CREATE TABLE `assoc_tags_images` (
   `assoc_id` int(11) NOT NULL,
-  PRIMARY KEY (`assoc_id`),
-  KEY `fk_image` (`image_id`),
-  KEY `fk_tag` (`tag_name`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `image_id` int(11) NOT NULL,
+  `tag_name` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `category`
+-- Table structure for table `category`
 --
 
-DROP TABLE IF EXISTS `category`;
-CREATE TABLE IF NOT EXISTS `category` (
-  `category_id` int(11) NOT NULL AUTO_INCREMENT,
-  `category_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`category_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-INSERT INTO `category` (`category_name`) VALUES
-('Vacances'),
-('Animaux');
-
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `images`
+-- Table structure for table `images`
 --
 
-DROP TABLE IF EXISTS `images`;
-CREATE TABLE IF NOT EXISTS `images` (
-  `image_id` int(11) NOT NULL AUTO_INCREMENT,
-  `file_name` varchar(255) DEFAULT NULL,
-  `file_url` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
+CREATE TABLE `images` (
+  `image_id` int(11) NOT NULL,
+  `file_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `file_url` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `category_id` int(11) DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`image_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Déchargement des données de la table `images`
---
-
-INSERT INTO `images` (`image_id`, `file_name`, `file_url`, `description`, `category_id`, `created_at`) VALUES
-(1, 'adobe', 'https://as1.ftcdn.net/jpg/02/12/43/28/500_F_212432820_Zf6CaVMwOXFIylDOEDqNqzURaYa7CHHc.jpg', 'hello it\'s me', 1, '2020-04-14 22:00:00'),
-(2, NULL, NULL, NULL, NULL, '2020-04-20 17:08:47'),
-(3, 'adobe', 'https://as1.ftcdn.net/jpg/02/12/43/28/500_F_212432820_Zf6CaVMwOXFIylDOEDqNqzURaYa7CHHc.jpg', 'hello it\'s me', NULL, '2020-04-20 17:13:46'),
-(4, 'adobe', 'https://as1.ftcdn.net/jpg/02/12/43/28/500_F_212432820_Zf6CaVMwOXFIylDOEDqNqzURaYa7CHHc.jpg', 'hello it\'s me', NULL, '2020-04-20 17:14:01'),
-(5, 'adobe', 'https://as1.ftcdn.net/jpg/02/12/43/28/500_F_212432820_Zf6CaVMwOXFIylDOEDqNqzURaYa7CHHc.jpg', 'hello it\'s me', NULL, '2020-04-20 20:20:26'),
-(6, 'adobe', 'https://as1.ftcdn.net/jpg/02/12/43/28/500_F_212432820_Zf6CaVMwOXFIylDOEDqNqzURaYa7CHHc.jpg', 'hello it\'s me', NULL, '2020-04-20 20:21:17'),
-(7, 'adobe', 'https://as1.ftcdn.net/jpg/02/12/43/28/500_F_212432820_Zf6CaVMwOXFIylDOEDqNqzURaYa7CHHc.jpg', 'hello it\'s me', NULL, '2020-04-14 22:00:00'),
-(8, 'adobe', 'https://as1.ftcdn.net/jpg/02/12/43/28/500_F_212432820_Zf6CaVMwOXFIylDOEDqNqzURaYa7CHHc.jpg', 'hello it\'s me', NULL, '2020-04-20 20:32:01'),
-(9, 'adobe', 'https://as1.ftcdn.net/jpg/02/12/43/28/500_F_212432820_Zf6CaVMwOXFIylDOEDqNqzURaYa7CHHc.jpg', 'hello it\'s me', NULL, '2020-04-21 09:09:13'),
-(10, 'adobe', 'https://as1.ftcdn.net/jpg/02/12/43/28/500_F_212432820_Zf6CaVMwOXFIylDOEDqNqzURaYa7CHHc.jpg', 'hello it\'s me', NULL, '2020-04-21 09:10:02'),
-(11, 'adobe', 'https://as1.ftcdn.net/jpg/02/12/43/28/500_F_212432820_Zf6CaVMwOXFIylDOEDqNqzURaYa7CHHc.jpg', 'hello it\'s megg', NULL, '2020-04-21 16:16:13'),
-(12, 'adobe', 'https://as1.ftcdn.net/jpg/02/12/43/28/500_F_212432820_Zf6CaVMwOXFIylDOEDqNqzURaYa7CHHc.jpg', 'hello it\'s megg', NULL, '2020-04-21 16:47:46');
+  `created_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `tags`
+-- Table structure for table `tags`
 --
 
-DROP TABLE IF EXISTS `tags`;
-CREATE TABLE IF NOT EXISTS `tags` (
-  `tag_name` varchar(155) NOT NULL,
-  PRIMARY KEY (`tag_name`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-COMMIT;
+CREATE TABLE `tags` (
+  `tag_name` int(11) NOT NULL,
+  `tag_title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `assoc_tags_images`
+--
+ALTER TABLE `assoc_tags_images`
+  ADD PRIMARY KEY (`assoc_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`image_id`);
+
+--
+-- Indexes for table `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`tag_name`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `assoc_tags_images`
+--
+ALTER TABLE `assoc_tags_images`
+  MODIFY `assoc_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `images`
+--
+ALTER TABLE `images`
+  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `tag_name` int(11) NOT NULL AUTO_INCREMENT;
